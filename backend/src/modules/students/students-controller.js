dfbcc37f3fa8e499f1c9daaa3951054878343ba1
CodesByMidnyt <100563026+3th-Enjay@users.noreply.github.com>
2025-03-26 14:25:45 +0000

    fixed backend controller

--- backend/src/modules/students/students-controller.js
+++ backend/src/modules/students/students-controller.js
@@ -2,27 +2,40 @@ const asyncHandler = require("express-async-handler");
 const { getAllStudents, addNewStudent, getStudentDetail, setStudentStatus, updateStudent } = require("./students-service");
 
 const handleGetAllStudents = asyncHandler(async (req, res) => {
-    //write your code
+    const students = await getAllStudents(req.query)
+    res.status(200).json(students) 
 
 });
 
 const handleAddStudent = asyncHandler(async (req, res) => {
-    //write your code
+    const request = await addNewStudent(req.body)
+    res.status(201).json(request)
 
 });
 
 const handleUpdateStudent = asyncHandler(async (req, res) => {
-    //write your code
+    const { id } = req.params;
+    const updatedData = { ...req.body, id };
+    const result = await updateStudent(updatedData);
+    res.status(200).json(result)
 
 });
 
 const handleGetStudentDetail = asyncHandler(async (req, res) => {
-    //write your code
+    const { id } = req.params;
+    const student = await getStudentDetail(id);
+    res.status(200).json(student)
 
 });
 
 const handleStudentStatus = asyncHandler(async (req, res) => {
-    //write your code
+    const { userId, reviewerId, status } = req.body;
+    if (!userId || !reviewerId || !status) {
+        res.status(400);
+        throw new Error("User ID, Reviewer ID, and Status are required");
+    }
+    const result = await setStudentStatus({ userId, reviewerId, status });
+    res.status(200).json(result);
 
 });
 
